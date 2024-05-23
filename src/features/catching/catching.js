9e4b36d34cc67621043c29aa6f6ecac1efc9d6b9
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-23 10:58:06 +0530

    Add src/features/catching/catching.js

---
+++ src/features/catching/catching.js
@@ -0,0 +1,11 @@
+const pokemonList = require('../../data/pokemonList');
+
+function catchPokemon(user) {
+  // Catching logic
+}
+
+function checkShinyEncounter() {
+  // Shiny encounter logic
+}
+
+module.exports = { catchPokemon, checkShinyEncounter };
